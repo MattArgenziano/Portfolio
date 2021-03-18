181a3813b46ed3717065da128e37d7d4a343eda4
Matt_Argenziano <burnt.cheese999@gmail.com>
2021-03-18 12:00:22 -0400

    First Commit

---
+++ dist/script.js
@@ -0,0 +1,31 @@
+// Calling scroll function
+window.onscroll = function() {
+  displayOnScroll()
+};
+
+// BACK TO TOP FUNCTION
+function backToTop() {
+  const topButton = document.querySelector('.back-to-top');
+
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+function displayOnScroll() {
+  const topButton = document.querySelector('.back-to-top');
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
