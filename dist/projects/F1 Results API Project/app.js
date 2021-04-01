355f91c7cec7b158b7329c4315023c71857fdec2
Matt_Argenziano <burnt.cheese999@gmail.com>
2021-04-01 12:05:16 -0400

    added full standing functionality

--- dist/projects/F1 Results API Project/app.js
+++ dist/projects/F1 Results API Project/app.js
@@ -1,10 +1,13 @@
 document.querySelector('.get-results').addEventListener('click', getResults);
 document.querySelector('.get-random').addEventListener('click', getRandom);
+document.querySelector('#full-standings').addEventListener('click', getFullStandings);
+
 
 
 function getResults(e){
     const year = document.getElementById('year').value;
     const round = document.getElementById('round').value;
+    let standingsDiv = document.querySelector('.hidden');
 
     const xhr = new XMLHttpRequest();
 
@@ -38,6 +41,8 @@ function getResults(e){
     xhr.send();
 
     e.preventDefault();
+
+    standingsDiv.style.visibility = 'visible';
 }
 
 function getRandom(e) {
@@ -45,6 +50,7 @@ function getRandom(e) {
     const round = document.getElementById('round');
     year.value = randomNum(1950, 2021);
     round.value = randomNum(1, 7);
+    let standingsDiv = document.querySelector('.hidden');
 
 
     const xhr = new XMLHttpRequest();
@@ -72,6 +78,52 @@ function getRandom(e) {
 
 
         } else {
+            // document.querySelector('.results').innerHTML = 'ERROR';
+        }
+    }
+
+    xhr.send();
+
+    e.preventDefault();
+
+    standingsDiv.style.visibility = 'visible';
+
+    getResults();
+}
+
+// FOR FUTURE ADDITION
+function getFullStandings(e){
+    const year = document.getElementById('year').value;
+    const round = document.getElementById('round').value;
+    const fullStandings = document.querySelector('#full-standings');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `https://ergast.com/api/f1/${year}/${round}/results.json`, true);
+
+    xhr.onload = function(){
+        if(this.status === 200) {
+            response = this.responseText;
+            let data = JSON.parse(response);
+            let position = data.MRData.RaceTable.Races[0].Results[0].number;
+            let races = data.MRData.RaceTable.Races;
+            let results = data.MRData.RaceTable.Races[0].Results;
+            let raceName = data.MRData.RaceTable.Races[0].raceName;
+            let standingsText = document.getElementById('full-standings-text');
+            
+            let output = '';
+            for (let i = 0; i < results.length; i++) {
+                
+
+                output += "<tr>"+ "<td>" + results[i].position + "</td>";
+                output += "<td>" + results[i].Driver.givenName + ' ' + results[i].Driver.familyName + "</td>";
+                output += "<td>" + results[i].Constructor.name + "</td>" + "</tr>";
+                document.getElementById('full-results').innerHTML = output;
+            } 
+
+            standingsText.textContent = `Full standings for the ${year} ${raceName}`;
+
+        } else {
             document.querySelector('.results').innerHTML = 'ERROR';
         }
     }
@@ -80,7 +132,6 @@ function getRandom(e) {
 
     e.preventDefault();
 
-    getResults();
 }
 
 function randomNum(min, max) {
